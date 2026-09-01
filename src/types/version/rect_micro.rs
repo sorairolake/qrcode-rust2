cc8b24d32e83e796cdf0bd961eda1d7cd636ea60
Shun Sakai <sorairolake@protonmail.ch>
2026-09-01 15:19:05 +0900

    Implement From and TryFrom for RectMicroVersion
    Added conversion implementations for RectMicroVersion.

--- src/types/version/rect_micro.rs
+++ src/types/version/rect_micro.rs
@@ -4,11 +4,13 @@
 
 //! Implementation of [`RectMicroVersion`].
 
+use crate::error::Error;
+
 /// `RectMicroVersion` is a type that represents a rMQR code version.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum RectMicroVersion {
     /// A 7×43 rMQR code symbol.
-    R7x43,
+    R7x43 = 1,
 
     /// A 7×59 rMQR code symbol.
     R7x59,
@@ -102,4 +104,85 @@ pub enum RectMicroVersion {
 
     /// A 17×139 rMQR code symbol.
     R17x139,
+}
+
+impl From<RectMicroVersion> for (u8, u8) {
+    fn from(version: RectMicroVersion) -> Self {
+        match version {
+            RectMicroVersion::R7x43 => (7, 43),
+            RectMicroVersion::R7x59 => (7, 59),
+            RectMicroVersion::R7x77 => (7, 77),
+            RectMicroVersion::R7x99 => (7, 99),
+            RectMicroVersion::R7x139 => (7, 139),
+            RectMicroVersion::R9x43 => (9, 43),
+            RectMicroVersion::R9x59 => (9, 59),
+            RectMicroVersion::R9x77 => (9, 77),
+            RectMicroVersion::R9x99 => (9, 99),
+            RectMicroVersion::R9x139 => (9, 139),
+            RectMicroVersion::R11x27 => (11, 27),
+            RectMicroVersion::R11x43 => (11, 43),
+            RectMicroVersion::R11x59 => (11, 59),
+            RectMicroVersion::R11x77 => (11, 77),
+            RectMicroVersion::R11x99 => (11, 99),
+            RectMicroVersion::R11x139 => (11, 139),
+            RectMicroVersion::R13x27 => (13, 27),
+            RectMicroVersion::R13x43 => (13, 43),
+            RectMicroVersion::R13x59 => (13, 59),
+            RectMicroVersion::R13x77 => (13, 77),
+            RectMicroVersion::R13x99 => (13, 99),
+            RectMicroVersion::R13x139 => (13, 139),
+            RectMicroVersion::R15x43 => (15, 43),
+            RectMicroVersion::R15x59 => (15, 59),
+            RectMicroVersion::R15x77 => (15, 77),
+            RectMicroVersion::R15x99 => (15, 99),
+            RectMicroVersion::R15x139 => (15, 139),
+            RectMicroVersion::R17x43 => (17, 43),
+            RectMicroVersion::R17x59 => (17, 59),
+            RectMicroVersion::R17x77 => (17, 77),
+            RectMicroVersion::R17x99 => (17, 99),
+            RectMicroVersion::R17x139 => (17, 139),
+        }
+    }
+}
+
+impl TryFrom<(u8, u8)> for NormalVersion {
+    type Error = Error;
+
+    fn try_from(version: (u8, u8)) -> Result<Self, Self::Error> {
+        match version {
+            (7, 43) => Ok(Self::R7x43),
+            (7, 59) => Ok(Self::R7x59),
+            (7, 77) => Ok(Self::R7x77),
+            (7, 99) => Ok(Self::R7x99),
+            (7, 139) => Ok(Self::R7x139),
+            (9, 43) => Ok(Self::R9x43),
+            (9, 59) => Ok(Self::R9x59),
+            (9, 77) => Ok(Self::R9x77),
+            (9, 99) => Ok(Self::R9x99),
+            (9, 139) => Ok(Self::R9x139),
+            (11, 27) => Ok(Self::R11x27),
+            (11, 43) => Ok(Self::R11x43),
+            (11, 59) => Ok(Self::R11x59),
+            (11, 77) => Ok(Self::R11x77),
+            (11, 99) => Ok(Self::R11x99),
+            (11, 139) => Ok(Self::R11x139),
+            (13, 27) => Ok(Self::R13x27),
+            (13, 43) => Ok(Self::R13x43),
+            (13, 59) => Ok(Self::R13x59),
+            (13, 77) => Ok(Self::R13x77),
+            (13, 99) => Ok(Self::R13x99),
+            (13, 139) => Ok(Self::R13x139),
+            (15, 43) => Ok(Self::R15x43),
+            (15, 59) => Ok(Self::R15x59),
+            (15, 77) => Ok(Self::R15x77),
+            (15, 99) => Ok(Self::R15x99),
+            (15, 139) => Ok(Self::R15x139),
+            (17, 43) => Ok(Self::R17x43),
+            (17, 59) => Ok(Self::R17x59),
+            (17, 77) => Ok(Self::R17x77),
+            (17, 99) => Ok(Self::R17x99),
+            (17, 139) => Ok(Self::R17x139),
+            _ => Err(Error::InvalidVersion),
+        }
+    }
 }
