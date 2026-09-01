b809105a3f0db40425d87920c7603252c2879c5d
Shun Sakai <sorairolake@protonmail.ch>
2026-09-02 07:52:02 +0900

    Update svg.rs

--- tests/svg.rs
+++ tests/svg.rs
@@ -5,11 +5,13 @@
 
 #![cfg(feature = "svg")]
 
-use qrcode2::{EcLevel, QrCode, Version, render::svg::Color};
+use qrcode2::{
+    EcLevel, MicroVersion, NormalVersion, QrCode, RectMicroVersion, Version, render::svg::Color,
+};
 
 #[test]
 fn annex_i_qr_as_svg() {
-    let code = QrCode::with_version(b"01234567", Version::Normal(1), EcLevel::M).unwrap();
+    let code = QrCode::with_version(b"01234567", Version::Normal(NormalVersion::V1), EcLevel::M).unwrap();
     let image = code.render::<Color<'_>>().build();
     let expected = include_str!("data/test_annex_i_qr_as_svg.svg");
     assert_eq!(&image, expected);
@@ -17,7 +19,7 @@ fn annex_i_qr_as_svg() {
 
 #[test]
 fn annex_i_micro_qr_as_svg() {
-    let code = QrCode::with_version(b"01234567", Version::Micro(2), EcLevel::L).unwrap();
+    let code = QrCode::with_version(b"01234567", Version::Micro(MicroVersion::M2), EcLevel::L).unwrap();
     let image = code
         .render()
         .min_dimensions(200, 200)
@@ -30,7 +32,7 @@ fn annex_i_micro_qr_as_svg() {
 
 #[test]
 fn annex_i_rmqr_as_svg() {
-    let code = QrCode::with_version(b"0123456", Version::RectMicro(11, 27), EcLevel::H).unwrap();
+    let code = QrCode::with_version(b"0123456", Version::RectMicro(RectMicroVersion::R11x27), EcLevel::H).unwrap();
     let image = code.render::<Color<'_>>().build();
     let expected = include_str!("data/test_annex_i_rmqr_as_svg.svg");
     assert_eq!(&image, expected);
