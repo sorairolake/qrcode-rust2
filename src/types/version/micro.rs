72ceb917d33ced16f802acb72f518c78b14357a2
Shun Sakai <sorairolake@protonmail.ch>
2026-09-01 15:00:17 +0900

    Implement From and TryFrom for MicroVersion
    Add conversion implementations for MicroVersion

--- src/types/version/micro.rs
+++ src/types/version/micro.rs
@@ -4,11 +4,13 @@
 
 //! Implementation of [`MicroVersion`].
 
+use crate::error::Error;
+
 /// `MicroVersion` is a type that represents a Micro QR code version.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum MicroVersion {
     /// An 11×11 Micro QR code symbol.
-    M1,
+    M1 = 1,
 
     /// A 13×13 Micro QR code symbol.
     M2,
@@ -18,4 +20,24 @@ pub enum MicroVersion {
 
     /// A 17×17 Micro QR code symbol.
     M4,
+}
+
+impl From<MicroVersion> for u8 {
+    fn from(version: MicroVersion) -> Self {
+        version as Self
+    }
+}
+
+impl TryFrom<u8> for MicroVersion {
+    type Error = Error;
+
+    fn try_from(version: u8) -> Result<Self, Self::Error> {
+        match version {
+            1 => Ok(Self::M1),
+            2 => Ok(Self::M2),
+            3 => Ok(Self::M3),
+            4 => Ok(Self::M4),
+            _ => Err(Error::InvalidVersion),
+        }
+    }
 }
