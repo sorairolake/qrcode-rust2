4ff46960f84fa11b37f2584dd69f2c89909ce46d
Shun Sakai <sorairolake@protonmail.ch>
2026-09-02 03:16:50 +0900

    Update documentation for ec_level in capacity.rs
    Fix documentation comment for ec_level usage.

--- src/ec/capacity.rs
+++ src/ec/capacity.rs
@@ -18,7 +18,7 @@ use crate::{
 /// # Errors
 ///
 /// Returns [`Err`] if it is not valid to use the `ec_level` for the given
-/// version (e.g. [`Version::Micro(1)`](Version::Micro) with [`EcLevel::H`]).
+/// version.
 ///
 /// # Examples
 ///
