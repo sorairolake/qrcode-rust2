783709fce0ce4aa350bdd49639babec114b708ce
Shun Sakai <sorairolake@protonmail.ch>
2026-09-02 07:50:19 +0900

    Refactor QR code version handling in tests

--- tests/pic.rs
+++ tests/pic.rs
@@ -5,11 +5,13 @@
 
 #![cfg(feature = "pic")]
 
-use qrcode2::{EcLevel, QrCode, Version, render::pic::Color};
+use qrcode2::{
+    EcLevel, MicroVersion, NormalVersion, QrCode, RectMicroVersion, Version, render::pic::Color,
+};
 
 #[test]
 fn annex_i_qr_as_pic() {
-    let code = QrCode::with_version(b"01234567", Version::Normal(1), EcLevel::M).unwrap();
+    let code = QrCode::with_version(b"01234567", Version::Normal(NormalVersion::V1), EcLevel::M).unwrap();
     let image = code.render::<Color>().build();
     let expected = include_str!("data/test_annex_i_qr_as_pic.pic");
     assert_eq!(&image, expected);
@@ -17,7 +19,7 @@ fn annex_i_qr_as_pic() {
 
 #[test]
 fn annex_i_micro_qr_as_pic() {
-    let code = QrCode::with_version(b"01234567", Version::Micro(2), EcLevel::L).unwrap();
+    let code = QrCode::with_version(b"01234567", Version::Micro(MicroVersion::M2), EcLevel::L).unwrap();
     let image = code.render::<Color>().min_dimensions(1, 1).build();
     let expected = include_str!("data/test_annex_i_micro_qr_as_pic.pic");
     assert_eq!(&image, expected);
@@ -25,7 +27,7 @@ fn annex_i_micro_qr_as_pic() {
 
 #[test]
 fn annex_i_rmqr_as_pic() {
-    let code = QrCode::with_version(b"0123456", Version::RectMicro(11, 27), EcLevel::H).unwrap();
+    let code = QrCode::with_version(b"0123456", Version::RectMicro(RectMicroVersion::R11x27), EcLevel::H).unwrap();
     let image = code.render::<Color>().build();
     let expected = include_str!("data/test_annex_i_rmqr_as_pic.pic");
     assert_eq!(&image, expected);
