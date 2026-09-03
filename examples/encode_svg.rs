0b6526f66cb4bc1b128e2ea382363ecfa4a0b87a
Shun Sakai <sorairolake@protonmail.ch>
2026-09-03 09:51:52 +0900

    Update encode_svg.rs

--- examples/encode_svg.rs
+++ examples/encode_svg.rs
@@ -82,7 +82,7 @@ impl From<Ecc> for EcLevel {
 
 #[derive(Clone, Debug, Default, ValueEnum)]
 enum Variant {
-    /// QR code.
+    /// Normal QR code.
     #[default]
     Normal,
 
@@ -101,8 +101,8 @@ fn main() -> anyhow::Result<()> {
     let code = if let Some(sv) = opt.symbol_version {
         let version = match opt.variant {
             Variant::Normal => {
-                let version =
-                    NormalVersion::try_from(sv[0]).context("could not set a QR code version")?;
+                let version = NormalVersion::try_from(sv[0])
+                    .context("could not set a normal QR code version")?;
                 Version::Normal(version)
             }
             Variant::Micro => {
