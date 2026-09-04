2593fd8638bcc9bb992341232db3b7a2aa4eae9f
Shun Sakai <sorairolake@protonmail.ch>
2026-09-05 08:51:53 +0900

    Remove As trait and associated macro implementations
    Removed the As trait and its implementation macro.

--- src/cast.rs
+++ src/cast.rs
@@ -15,31 +15,3 @@ impl Truncate for u16 {
         self as u8
     }
 }
-
-#[expect(clippy::wrong_self_convention)]
-pub trait As {
-    fn as_usize(self) -> usize;
-}
-
-macro_rules! impl_as {
-    ($ty:ty) => {
-        #[cfg(debug_assertions)]
-        impl As for $ty {
-            fn as_usize(self) -> usize {
-                usize::try_from(self).unwrap()
-            }
-        }
-
-        #[cfg(not(debug_assertions))]
-        impl As for $ty {
-            fn as_usize(self) -> usize {
-                self as usize
-            }
-        }
-    };
-}
-impl_as!(i16);
-impl_as!(isize);
-impl_as!(u8);
-impl_as!(u32);
-impl_as!(usize);
