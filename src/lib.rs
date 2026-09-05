ca60bbb9f63b69b08f4be90e86694f9d7f1efc68
Shun Sakai <sorairolake@protonmail.ch>
2026-09-05 11:32:52 +0900

    Fix type conversion in is_functional method

--- src/lib.rs
+++ src/lib.rs
@@ -414,7 +414,7 @@ impl QrCode {
     pub fn is_functional(&self, x: usize, y: usize) -> bool {
         let x = x.try_into().unwrap();
         let y = y.try_into().unwrap();
-        canvas::is_functional(self.version, self.version.width(), x, y)
+        canvas::is_functional(self.version, self.version.width().into(), x, y)
     }
 
     /// Converts the QR code into a human-readable string. This is mainly for
