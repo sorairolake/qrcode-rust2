9c3b10741d25055ae8e065b0382d1247efc89f9d
Shun Sakai <sorairolake@protonmail.ch>
2026-09-05 09:42:00 +0900

    refactor: remove `Truncate` trait

--- src/cast.rs
+++ src/cast.rs
@@ -11,6 +11,8 @@ pub trait Truncate {
 
 impl Truncate for u16 {
     #[expect(clippy::cast_possible_truncation)]
+    // TODO: use `u16::truncate()` when stable.
+    // <https://github.com/rust-lang/rust/issues/154330>
     fn truncate_as_u8(self) -> u8 {
         self as u8
     }
